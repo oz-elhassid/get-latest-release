1b144b302ea15fb281a0625d5d8731def92cce07
Oz Elhassid <45009449+oz-elhassid@users.noreply.github.com>
2021-12-07 18:21:59 +0200

    support regex as input (#1)

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let owner = core.getInput('owner');
 let repo = core.getInput('repo');
 const and_filters = core.getInput('and_filters').trim().split(",").map(x => x.trim());
 const token = core.getInput('token');
+const regex_filters = core.getInput('regex_filters').trim().split(",").map(x => x.trim());
 
 const octokit = (token ? new Octokit({ auth: token }) : new Octokit());
 
@@ -20,11 +21,23 @@ async function run() {
             per_page: 100,
         });
         releases = releases.data;
-        and_filters.forEach(element => {
-            let [key, value] = element.split(":").map(x => x.trim());
-            if (key)
-                releases = releases.filter(x => x[key].toString().includes(value));
-        });
+        if (and_filters.length)
+            and_filters.forEach(element => {
+                let [key, value] = element.split(":").map(x => x.trim());
+                if (key)
+                    releases = releases.filter(x => x[key].toString().includes(value));
+            });
+        if (regex_filters.length)
+            regex_filters.forEach(element => {
+                let [key, value] = element.split(":").map(x => x.trim());
+                if (key) {
+                    if (value[0] != '/') core.setFailed('invalid regex');
+                    const i = value.lastIndexOf('/');
+                    if (i <= 0) core.setFailed('invalid regex');
+                    const regex = new RegExp(value.slice(1, i), value.slice(i+1));
+                    releases = releases.filter(x => regex.test(x[key].toString()));
+                }
+            });
         if (releases.length) {
             core.setOutput('url', releases[0].url);
             core.setOutput('assets_url', releases[0].assets_url);
@@ -45,7 +58,6 @@ async function run() {
             core.setOutput('author_html_url', releases[0].author.html_url);
             core.setOutput('author_type', releases[0].author.type);
             core.setOutput('author_site_admin', releases[0].author.site_admin.toString());
-            core.setOutput('release_url', releases[0].html_url + '/releases/tag/' + releases[0].name);
             core.info("Latest release tag: " + releases[0].tag_name);
         } else {
             core.setFailed("No valid releases");
